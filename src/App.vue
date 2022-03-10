255e403b658f34a7505eae1cef5f7eac3af7967f
yul <1103221645@qq.com>
2022-03-10 16:27:03 +0800

    模板构建商品

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,58 @@
 <template>
-  <HelloWorld>
-    <template #default="slotProps">
-      <p>{{ slotProps.item }}</p>
-    </template>
-  </HelloWorld>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <div>
+      <h4>新增商品</h4>
+      <input type="text" v-model="item" @keyup.enter="addItem" />
+      <button @click="addItem">添加</button>
+    </div>
+
+    <div>
+      <h5>商品列表</h5>
+      <p
+        v-for="i in itemList"
+        :key="i"
+        :class="['item', { active: currentItem === i }]"
+        @click="currentItem = i"
+      >
+        {{ i }}
+      </p>
+      <div>总数：{{ total }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
-
   export default {
     name: 'App',
-    components: {
-      HelloWorld,
-    },
-    provide: { msg: '123123' },
     data() {
       return {
-        msg: 'father',
+        title: '购物车',
+        itemList: ['🍎', '🍌', '🧆'],
+        item: '',
+        currentItem: '',
       }
+    },
+    computed: {
+      total() {
+        return this.itemList.length
+      },
+    },
+    methods: {
+      addItem() {
+        this.itemList.push(this.item)
+        this.item = ''
+      },
     },
   }
 </script>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+<style scoped>
+  .item {
+    cursor: pointer;
+  }
+  .active {
+    background-color: #838504;
   }
 </style>
