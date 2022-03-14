73315cebcc37371e11deb1abce83b6d89076f66d
yul <1103221645@qq.com>
2022-03-14 09:07:30 +0800

    列表过度

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h5>商品列表</h5>
-    <p
-      v-for="(i, index) in itemList"
-      :key="index"
-      :class="['item', { active: currentItem === index }]"
-      @click="currentItem = index"
-    >
-      {{ i }}
-    </p>
+    <transition-group name="fade">
+      <p
+        v-for="(i, index) in itemList"
+        :key="index"
+        :class="['item', { active: currentItem === index }]"
+        @click="currentItem = index"
+      >
+        {{ i }}
+      </p>
+    </transition-group>
+
     <div>总数：{{ total }}</div>
   </div>
 </template>
@@ -30,3 +33,14 @@
     },
   }
 </script>
+
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s ease;
+  }
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
